12caf7c7739695e52042f3f7176b267997289dee
David S. <donnydavidspiegel@gmail.com>
2025-05-04 13:09:35 -0700

    Worked on stuff

--- src/model/OS/Compiler/Scanner.py
+++ src/model/OS/Compiler/Scanner.py
@@ -1,13 +1,13 @@
 """
-Name
-
+Name: Scanner.py
+Purpose: To take a string input and tokenize it. 
+Notes:
+        * I am rewriting this code for it to be more readable and also faster.
 """
-
 
 import os
 import traceback
 import subprocess
-
 
 class Token:
     def __init__(self,kind,val):
@@ -22,8 +22,31 @@ class Scanner:
         self.pos = 0
         self.current_char = self.text[self.pos] if len(self.text) > 0 else None
 
-    #def advance(self,optional=None):
+    def advance(self,optional=None):
+        if optional is None:
+            self.pos += 1
+        else:
+            self.pos += len(optional)
+        if self.pos > len(self.text)-1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
 
+    # Removed checking for ',' because there is a better way.
+    """def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()"""
+    # Going to try to do this all with one module
+    def scan (self,text):
+        self.current_char = self.text[self.pos] if len(self.text) > 0 else None
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.advance()
+
+
+    
+
+    
 def main():
     t = Token("STRING","HEY")
     print("What the string is: ",t)
