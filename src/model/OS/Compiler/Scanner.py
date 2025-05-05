714a3e71dae040e47ea9da9372d68a9b2aa39a3e
David Spiegel <davidspiegel@Davids-MacBook-Air.local>
2025-05-04 20:21:53 -0700

    Working on scanner.

--- src/model/OS/Compiler/Scanner.py
+++ src/model/OS/Compiler/Scanner.py
@@ -3,11 +3,19 @@ Name: Scanner.py
 Purpose: To take a string input and tokenize it. 
 Notes:
         * I am rewriting this code for it to be more readable and also faster.
+        * I am also making the 'ls' and 'cd' other functions that can run inside the kernel instead of here.
+
 """
 
 import os
 import traceback
 import subprocess
+
+# Going to try and define some regex and see if that works:
+KEYWORDS = ['if','else','for','in','do']
+keyReg = r'\b(?:' + '|'.join(KEYWORDS) + r')\b'
+
+
 
 class Token:
     def __init__(self,kind,val):
@@ -36,19 +44,32 @@ class Scanner:
     """def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()"""
+    
     # Going to try to do this all with one module
-    def scan (self,text):
+    def next_token (self):
         self.current_char = self.text[self.pos] if len(self.text) > 0 else None
         while self.current_char is not None:
             if self.current_char.isspace():
+                while self.current_char.isspace():
+                    self.advance()
+            # Getting a number
+            if self.current_char.isdigit():
+                num = ''
+                while self.current_char is not None and self.current_char.isdigit():
+                    num += self.current_char
+                    self.advance()
+                return Token('NUMBER',int(num))
+
+    
+            # Possibly delete this!
+            else:
                 self.advance()
-
-
-    
-
-    
+                
 def main():
-    t = Token("STRING","HEY")
-    print("What the string is: ",t)
+    #t = Token("STRING","HEY")
+    script = '''x=10;'''
+    sc = Scanner(script)
+    print(sc.next_token())
+    #print("What the string is: ",t)
 
 main()
